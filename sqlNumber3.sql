223a906bdc2c6f31b7209dfbaaeffe1ea94f3697
marc wang <marc.wang@mail.mcgill.com>
2017-02-19 22:38:47 -0500

    replace " with ', apparently it does matter

--- sqlNumber3.sql
+++ sqlNumber3.sql
@@ -2,52 +2,60 @@
 
 INSERT INTO Members (mname, HouseNum , Street, City, PostalCode, phone, regidate)
 VALUES 
-("dummy mcd", "12","Rue Jeane-mance","Montreal", "8Y3R98", "123456789", "2014-01-08" );
+('dummy mcd', '12','Rue Jeane-mance','Montreal', '8Y3R98', '123456789', '2014-01-08' );
 
 
 INSERT INTO Members (mname, HouseNum , Street, City, PostalCode, phone, regidate)
 VALUES 
-("dummy mcd2", "13","Rue popo","Laval", "H7K3B7", "123456789", "2013-01-08" );
+('dummy mcd2', '13','Rue popo','Laval', 'H7K3B7', '123456789', '2013-01-08' );
 
 INSERT INTO Members (mname, HouseNum , Street, City, PostalCode, phone, regidate)
 VALUES 
-("dummy mcd2", "13","Rue popo","Laval", "H7K3B7", "123456789", "2013-01-08" );
+('dummy mcd2', '13','Rue popo','Laval', 'H7K3B7', '123456789', '2013-01-08' );
 
 INSERT INTO Members (mname, HouseNum , Street, City, PostalCode, phone, regidate)
 VALUES 
-("dummy mcd3", "14","Rue lala","ST-CLINCLIN", "P9U2B6", "123456789", "2012-01-08" );
+('dummy mcd3', '14','Rue lala','ST-CLINCLIN', 'P9U2B6', '123456789', '2012-01-08' );
 
 INSERT INTO Members (mname, HouseNum , Street, City, PostalCode, phone, regidate)
 VALUES 
-("dummy mcd5", "12","Rue boubou","ST-LALA", "T6B5D2", "123456789", "2011-01-08" );
+('dummy mcd5', '12','Rue boubou','ST-LALA', 'T6B5D2', '123456789', '2011-01-08' );
 
-INSERT INTO Plans (pname, cost, payment_frequency)
+
+SELECT * FROM Members;
+
+
+INSERT INTO Plans(pname, cost, payment_frequency)
 VALUES 
-("regular","$50", 1);
+('regular','$50', 1);
 
-INSERT INTO Plans (pname, cost, payment_frequency)
+INSERT INTO Plans(pname, cost, payment_frequency)
 VALUES 
-("Fancy","$50", 6);
+('Fancy','$50', 6);
 
-INSERT INTO Membership(mid, pname , pregidate)
+SELECT * FROM Plans;
+
+INSERT INTO Memberships(mid, pname , pregidate)
 VALUES 
-(1, "Fancy","2011-01-08" );
+(1, 'Fancy','2011-01-08' );
 
-INSERT INTO Membership(mid, pname , pregidate)
+INSERT INTO Memberships(mid, pname , pregidate)
 VALUES 
-(2, "regular","2011-01-08" );
+(2, 'regular','2011-01-08' );
 
 
-INSERT INTO Membership(mid, pname , pregidate)
+INSERT INTO Memberships(mid, pname , pregidate)
 VALUES 
-(3, "Fancy","2011-01-08" );
+(3, 'Fancy','2011-01-08' );
 
 
-INSERT INTO Membership(mid, pname , pregidate)
+INSERT INTO Memberships(mid, pname , pregidate)
 VALUES 
-(4, "regular","2011-01-08" );
+(4, 'regular','2011-01-08' );
 
 
-INSERT INTO Membership(mid, pname , pregidate)
+INSERT INTO Memberships(mid, pname , pregidate)
 VALUES 
-(5, "regular","2011-01-08" )
+(5, 'regular','2011-01-08' );
+
+SELECT * FROM Memberships;
