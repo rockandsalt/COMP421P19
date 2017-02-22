fb4a2eed744898f7a9d2c94bd44c6cde4c005edb
camhjohn <camhjohn@gmail.com>
2017-02-22 18:46:37 -0500

    commit plan

---
+++ plans.sql
@@ -0,0 +1,3 @@
+INSERT INTO Plans (pname, cost, payment_frequency) VALUES ('Regular', '$50', 1);
+INSERT INTO Plans (pname, cost, payment_frequency) VALUES ('Private','$120', 6);
+INSERT INTO Plans (pname, cost, payment_frequency) VALUES ('Premium','$100', 6);
