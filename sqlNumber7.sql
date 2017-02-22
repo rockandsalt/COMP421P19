97b7dd4efe387c87b529114702660f571e3a62ac
marc wang <marc.wang@mail.mcgill.com>
2017-02-21 23:12:31 -0500

    finished the query

--- sqlNumber7.sql
+++ sqlNumber7.sql
@@ -14,3 +14,17 @@ SELECT pname, payment_frequency * cost AS YearlyIncome
     FROM Income;
 
 
+CREATE VIEW ClassPopulation 
+AS
+WITH ClassPop as (SELECT COUNT(*) as NUM, classID as CId
+    FROM ClassEnroll
+    GROUP BY CId
+)
+SELECT  CId, NUM,Cname, CMaxEnroll 
+    FROM ClassPop NATURAL JOIN Classes;
+
+SELECT * 
+    FROM ClassPopulation
+    WHERE (CAST(NUM AS FLOAT)/CMaxEnroll*100.0) = (SELECT MAX(CAST(NUM AS FLOAT)/CMaxEnroll*100.0) FROM ClassPopulation);
+
+
